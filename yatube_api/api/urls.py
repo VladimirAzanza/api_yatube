6601edeb48735699ce6295dd2376096a18a8071c
Vladimir Azanza <vladimirazanza@gmail.com>
2024-07-29 12:40:34 +0300

    Create Post View Set

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.urls import include, path
 from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
+
+from api.views import PostViewSet
+
+app_name = 'api'
+router = DefaultRouter()
+router.register('posts', PostViewSet, basename='post')
 
 urlpatterns = [
     path('api-token-auth/', views.obtain_auth_token),
+    path('', include(router.urls))
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .serializers import PostSerializer
+from posts.models import Post
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(author=self.request.user)
