3593c97e88b685126b744a52aba17854aeae1b62
Vladimir Azanza <vladimirazanza@gmail.com>
2024-07-29 13:32:44 +0300

    Change the method field for author at post

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from posts.models import Group, Post
+from posts.models import Group, Post, User
 
 
 class GroupSerializer(serializers.ModelSerializer):
@@ -10,9 +10,15 @@ class GroupSerializer(serializers.ModelSerializer):
 
 
 class PostSerializer(serializers.ModelSerializer):
-    group = GroupSerializer()
+    group = serializers.PrimaryKeyRelatedField(
+        queryset=Group.objects.all()
+    )
+    author = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
         fields = '__all__'
         read_only_fields = ('author',)
+
+    def get_author(self, obj):
+        return obj.author.first_name
