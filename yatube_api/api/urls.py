d384395c4256c98355235e1c0b2509d4196d131b
Vladimir Azanza <vladimirazanza@gmail.com>
2024-08-01 11:10:08 +0300

    Change urls name to indicate the version of the API

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,33 +1,21 @@
 from django.urls import include, path
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
-# from rest_framework_nested import routers
 
 from api.views import CommentViewSet, GroupViewSet, PostViewSet
 
-app_name = 'api'
+app_name = 'api_v1'
 
-router = DefaultRouter()
-router.register('posts', PostViewSet, basename='post')
-router.register('groups', GroupViewSet, basename='group')
-# Не знаю, использовать ли регулярное выражение для урл или
-# библиотеку rest_framework_nested.routers, чтобы сократить код.
-
-router.register(
+router_v1 = DefaultRouter()
+router_v1.register('posts', PostViewSet, basename='post')
+router_v1.register('groups', GroupViewSet, basename='group')
+router_v1.register(
     r'posts/(?P<post_id>\d+)/comments',
     CommentViewSet,
     basename='comment_post'
 )
 
-# comment_router = routers.NestedSimpleRouter(
-#    router,
-#    r'posts',
-#    lookup='post'
-# )
-# comment_router.register(r'comments', CommentViewSet, basename='comment-post')
-
 urlpatterns = [
-    path('api-token-auth/', views.obtain_auth_token),
-    path('', include(router.urls)),
-    # path('', include(comment_router.urls))
+    path('v1/api-token-auth/', views.obtain_auth_token),
+    path('v1/', include(router_v1.urls)),
 ]
