b4a291ba2bef4d90ef02e63b1e326536443cc180
Vladimir Azanza <vladimirazanza@gmail.com>
2024-08-02 10:22:13 +0300

    Create file mixin

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.exceptions import PermissionDenied
-from rest_framework.viewsets import (ModelViewSet, ReadOnlyModelViewSet)
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
+from .mixin import OnlyAuthorMixinViewSet
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
 from posts.models import Group, Post
 
@@ -9,18 +9,6 @@ from posts.models import Group, Post
 class GroupViewSet(ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-
-
-class OnlyAuthorMixinViewSet(ModelViewSet):
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужого контента запрещено!')
-        super().perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        if instance.author != self.request.user:
-            raise PermissionDenied('Удаление чужого контента запрещено!')
-        super().perform_destroy(instance)
 
 
 class PostViewSet(OnlyAuthorMixinViewSet):
